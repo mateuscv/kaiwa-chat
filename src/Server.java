e7e93756ec9fc0ece68c8f751e50cd36f4586c90
MateusAlvesP <mateusp2000@hotmail.com>
2020-11-30 10:54:06 -0300

    Eureka!

--- src/Server.java
+++ src/Server.java
@@ -1,2 +1,20 @@
-public class Server {
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.io.Writer;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.ArrayList;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JTextField;
+
+public class Server extends Thread {
+    public static void main(String[] args){
+        System.out.println("Eureka!");
+    }
 }
