734f611fb991bf87f600fe87ef3d56fc7c05c749
MateusAlvesP <mateusp2000@hotmail.com>
2020-11-30 11:37:03 -0300

    Pronto, só entregar

--- src/Server.java
+++ src/Server.java
@@ -13,8 +13,87 @@ import javax.swing.JLabel;
 import javax.swing.JOptionPane;
 import javax.swing.JTextField;
 
+
 public class Server extends Thread {
-    public static void main(String[] args){
-        System.out.println("Eureka!");
+    private static ArrayList<BufferedWriter>clients;
+    private static ServerSocket server;
+    private String name;
+    private Socket talk;
+    private InputStream in;
+    private InputStreamReader inr;
+    private BufferedReader bfr;
+
+    public Server(Socket con){
+        this.talk = con;
+        try {
+            in  = con.getInputStream();
+            inr = new InputStreamReader(in);
+            bfr = new BufferedReader(inr);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
+    public void sendToAll(BufferedWriter bwSaida, String msg) throws  IOException
+    {
+        BufferedWriter bwS;
+
+        for(BufferedWriter bw : clients){
+            bwS = (BufferedWriter)bw;
+            if(!(bwSaida == bwS)){
+                bw.write(name + " -> " + msg+"\r\n");
+                bw.flush();
+            }
+        }
+    }
+
+    public void run(){
+
+        try{
+
+            String msg;
+            OutputStream ou =  this.talk.getOutputStream();
+            Writer ouw = new OutputStreamWriter(ou);
+            BufferedWriter bfw = new BufferedWriter(ouw);
+            clients.add(bfw);
+            name = msg = bfr.readLine();
+
+            while(!"Sair".equalsIgnoreCase(msg) && msg != null)
+            {
+                msg = bfr.readLine();
+                sendToAll(bfw, msg);
+                System.out.println(msg);
+            }
+
+        }catch (Exception e) {
+            e.printStackTrace();
+
+        }
+    }
+
+    public static void main(String[] args){
+        try{
+            //Cria os objetos necessário para instânciar o servidor
+            JLabel lblMessage = new JLabel("Porta do Servidor:");
+            JTextField txtPorta = new JTextField("12345");
+            Object[] texts = {lblMessage, txtPorta };
+            JOptionPane.showMessageDialog(null, texts);
+            server = new ServerSocket(Integer.parseInt(txtPorta.getText()));
+            clients = new ArrayList<BufferedWriter>();
+            JOptionPane.showMessageDialog(null,"Servidor ativo na porta: "+
+                    txtPorta.getText());
+
+            while(true){
+                System.out.println("Aguardando conexão...");
+                Socket con = server.accept();
+                System.out.println("Cliente conectado...");
+                Thread t = new Server(con);
+                t.start();
+            }
+
+        }catch (Exception e) {
+
+            e.printStackTrace();
+        }
+    }// Fim do método main
+
 }
